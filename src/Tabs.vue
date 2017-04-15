3d7e522fda0e802ecedf7ace2e86d505127afc63
Benny L.E.P <bendo01@gmail.com>
2017-04-15 23:48:37 +0800

    fix

--- src/Tabs.vue
+++ src/Tabs.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
-        <div class="tabs">
+        <div class="tabs" :class="predefinedClass">
             <ul>
-                <li v-for="tab in tabs"><a href="#">{{tab.name}}</a></li>
+                <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+                    <a :href="tab.href" @click.prevent="selectTab(tab)">{{ tab.name }}</a>
+                </li>
             </ul>
         </div>
         <div class="tabs-details">
@@ -13,16 +15,22 @@
 <script>
 'use strict';
 import Vue from 'vue';
+import Tab from './Tab'
 export default {
     data () {
         return {
             tabs: []
         }
     },
-    created() {
+    props: {
+        predefinedClass: {
+            type: String,
+            default: ''
+        }
+    },
+    mounted() {
         this.tabs = this.$children;
     },
-
     methods: {
         selectTab(selectedTab) {
             this.tabs.forEach(tab => {
